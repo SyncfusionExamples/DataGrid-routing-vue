238a61776980194f01bb1d5b6717dbdac2f0cecb
jmfarook7 <93311100+jmfarook7@users.noreply.github.com>
2023-07-03 12:22:09 +0530

    Create main.js

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+ import App from './App.vue'
+// import Tooltip from './tooltip.vue'
+import router from './router'
+
+new Vue({
+  router,
+  el: '#app1',
+  render: h => h(App)
+})
